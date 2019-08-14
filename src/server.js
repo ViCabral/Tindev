d8c2f1922ed7b6bc2c19f988876c8e912bc50583
ViCabral <vinicius.cabral@infojrunesp.com.br>
2019-08-14 00:30:26 -0300

    Correção Erros

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const server = express();
+
+mongoose.connect('mongodb+srv://Omnistack:omnistack@cluster0-cfbfu.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 
 server.use(express.json());
 server.use(routes);
