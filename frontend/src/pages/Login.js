9e91ed1a12b2a8f5eed081a886fe47cb8f0023a9
ViCabral <vinicius.cabral@infojrunesp.com.br>
2019-08-21 11:26:02 -0300

    Corrigindo problemas Frontend

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg';
 
+export default function Login({ history }) {
+  const [username, setUsername] = useState('');
 
-export default function login() {
-    return (
-        <div className="login-container">
-            <form>
-            <img src={logo} alt="Tindev"/>
-            <input placeholder="Digite seu usuário Githb"/>
-            <button type="submit">Enviar</button>
-            </form>
-        </div>
-    );
-    
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      username,
+    });
+
+    const { _id } = response.data;
+
+    history.push(`/dev/${_id}`);
+  }
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt="Tindev"/>
+        <input 
+          placeholder="Digite seu usuário no Github"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
 }
